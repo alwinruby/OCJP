d7865f24f3c6672fe50f29e1896451aa543ee3e1
asolanky <alwinsolanky@gmail.com>
2018-03-20 12:23:43 +0000

    Primative types in Java continued

--- Learning/src/Types.java
+++ Learning/src/Types.java
@@ -51,7 +51,23 @@ public class Types {
              
 
               // 'A' + 'B' + 'C'; Strings are just a collection or sequence of characters
-
+              
+              //Edge case
+              int a = 2;
+              int b = 3;
+              int c = a/b; // 2/3 = 0.66666 --> 0
+              int d = b/a; // 3/2 = 1.5 -> 1
+              
+              System.out.println(c);
+              System.out.println(d);
+              
+              int f = 20;
+              double r = 22;
+              int result2 = (int) (f/r);
+              double result3 = f/r ;
+            
+              System.out.println(result2);
+              System.out.println(result3);
        }
 
 }
