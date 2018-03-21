0659a5f46a5063133aa42f6a45db6f5efe636d45
asolanky <alwinsolanky@gmail.com>
2018-03-21 14:23:25 +0000

    arrays part one

---
+++ Learning/src/Arrays.java
@@ -0,0 +1,19 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int[] myArray = new int[4];
+		myArray[0] = 20;
+		myArray[1] = 30;
+		myArray[2] = 2;
+		myArray[3] = 4;
+		
+		for (int i=0; i < myArray.length; i++)
+		{
+			System.out.println(myArray[i]);
+		}
+		
+	}
+
+}
